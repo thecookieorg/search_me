1e41b44895877e3908b7736822162ea6ef4155e9
thecookieorg <marko.manojlovic.bg@gmail.com>
2020-12-28 12:48:14 -0800

    added pages/index and geocoder to restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,9 @@
 class Restaurant < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
-  
+
+  geocoded_by :address
+
   has_many :groups
   has_many :categories, through: :groups
 end
